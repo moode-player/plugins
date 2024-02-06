39985926509f6be17a70c49c9077e2c4b53ed863
Tim Curtis <tim@moodeaudio.org>
2024-02-06 15:10:49 -0500

    Update CamillaDSP v2-sample-configs

--- camilladsp/v2-sample-configs/update/install.sh
+++ camilladsp/v2-sample-configs/update/install.sh
@@ -82,7 +82,8 @@ apt -y install systemd-timesyncd
 # 2 - Install plugin files
 STEP=$((STEP + 1))
 message_log "** Step $STEP-$TOTAL_STEPS: Install $PLUGIN_NAME"
-cp update/files/* /usr/share/camilladsp/configs
+cp update/files/configs* /usr/share/camilladsp/configs
+cp update/files/coeffs* /usr/share/camilladsp/coeffs
 
 # 3 - Flush cached disk writes
 STEP=$((STEP + 1))
